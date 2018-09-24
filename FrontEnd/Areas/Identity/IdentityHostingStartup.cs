e7835fdee980d68a2100eb7394d90f2202474c4f
Glenn Strachan <strachan.glenn@gmail.com>
2018-09-24 12:17:52 +0100

    Added Identity

---
+++ FrontEnd/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,42 @@
+﻿using FrontEnd.Areas.Identity.Data;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FrontEnd.Areas.Identity.IdentityHostingStartup))]
+namespace FrontEnd.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<FrontEndIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FrontEndIdentityContextConnection")));
+
+                services.AddDefaultIdentity<FrontEndUser>()
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<FrontEndIdentityContext>();
+
+                Microsoft.AspNetCore.Authentication.AuthenticationBuilder authBuilder = services.AddAuthentication();
+
+                IConfigurationSection twitterConfig = context.Configuration.GetSection("twitter");
+                if (twitterConfig["consumerKey"] != null)
+                {
+                    authBuilder.AddTwitter(options => twitterConfig.Bind(options));
+                }
+
+                IConfigurationSection googleConfig = context.Configuration.GetSection("google");
+                if (googleConfig["clientID"] != null)
+                {
+                    authBuilder.AddGoogle(options => googleConfig.Bind(options));
+                }
+            });
+        }
+    }
+}
