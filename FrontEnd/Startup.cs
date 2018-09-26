05a5e558a2a56a900b3c8b6543c72ebaefc5b143
Glenn Strachan <strachan.glenn@gmail.com>
2018-09-26 15:51:21 +0100

    added serilog

--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -92,9 +87,7 @@ namespace FrontEnd
             }
 
             app.UseStaticFiles();
-
             app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
